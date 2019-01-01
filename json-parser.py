0171582eb962306469bc3b0c454364d4458e3423
unknown <platonfortisb@gmail.com>
2019-01-01 12:56:20 -0800

    ??

--- json-parser.py
+++ json-parser.py
@@ -1,15 +1,39 @@
+'''
+ia scriptul cela care-i in git la tine si fa urmatoarele:
+- afiseaza cel mai lung email (ma refer la lunginea in caractere)
+- afiseaza ID-ul care corespunde celui mai lung email
+cind te convingi ca lucreaza, fa commit si eu o sa fac review
+
+'''
+
+
 import requests
 
-URL = 'https://jsonplaceholder.typicode.com/comments'
+email = 0
 
-response = requests.get(URL)
+URL = 'https://jsonplaceholder.typicode.com/comments' 
+
+response = requests.get(URL) 
 
 responseBody = response.json()
 responseLength = len(responseBody)
 
 if responseLength:
-    for element in responseBody:
-        print '%d --> %s' % (element['id'], element['email'])
-else:
-    print('Error: empty response!')
+	for element in responseBody:
+		email_counter = 0
+		if len(element['email']) >= email_counter:
+			email_counter = len(element['email'])
+			if email_counter >= email:
+				email = email_counter
 
+for element in responseBody:
+	if len(element['email']) == email:
+		print('cel mai mare email: ',element['email'])
+		print('id: ',element['id'])
+
+
+			
+
+
+
+				
