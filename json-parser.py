3a76ad8753a535a3e14723884d30668ce6f433f1
unknown <platonfortisb@gmail.com>
2019-01-02 02:48:25 -0800

    !!

--- json-parser.py
+++ json-parser.py
@@ -10,6 +10,8 @@ cind te convingi ca lucreaza, fa commit si eu o sa fac review
 import requests
 
 email = 0
+email_counter = 0
+id = 0
 
 URL = 'https://jsonplaceholder.typicode.com/comments' 
 
@@ -20,16 +22,13 @@ responseLength = len(responseBody)
 
 if responseLength:
 	for element in responseBody:
-		email_counter = 0
-		if len(element['email']) >= email_counter:
-			email_counter = len(element['email'])
-			if email_counter >= email:
-				email = email_counter
+		if len(element['email']) > email_counter:  
+			email_counter = len(element['email'])    
+			email = element['email']
+			id = element['id']
 
-for element in responseBody:
-	if len(element['email']) == email:
-		print('cel mai mare email: ',element['email'])
-		print('id: ',element['id'])
+print('cel mai lung email : ', email, 'id: ', id)			
+	
 
 
 			
