1b4dd5aa855c8da22dee7d55dbda750bd0d17302
yavin <34816301+yawyn@users.noreply.github.com>
2022-03-29 22:51:32 +0300

    Update json-parser.py

--- json-parser.py
+++ json-parser.py
@@ -1,38 +1,16 @@
-'''
-ia scriptul cela care-i in git la tine si fa urmatoarele:
-- afiseaza cel mai lung email (ma refer la lunginea in caractere)
-- afiseaza ID-ul care corespunde celui mai lung email
-cind te convingi ca lucreaza, fa commit si eu o sa fac review
-
-'''
-
-
 import requests
 
-email = 0
-email_counter = 0
-id = 0
+URL = 'https://jsonplaceholder.typicode.com/comments'
 
-URL = 'https://jsonplaceholder.typicode.com/comments' 
-
-response = requests.get(URL) 
+# Making the request
+response = requests.get(URL)
 
 responseBody = response.json()
+print type(responseBody)
 responseLength = len(responseBody)
 
 if responseLength:
-	for element in responseBody:
-		if len(element['email']) > email_counter:  
-			email_counter = len(element['email'])    
-			email = element['email']
-			id = element['id']
-
-print('cel mai lung email : ', email, 'id: ', id)			
-	
-
-
-			
-
-
-
-				
+    for element in responseBody:
+        print '%d --> %s' % (element['id'], element['email'])
+else:
+    print 'Error: empty response!'
